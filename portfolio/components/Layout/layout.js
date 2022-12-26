ac3cb95a0ed2bfad531f8a1a55056dbfc4730257
Vikash <yadav621gzp@gmail.com>
2022-12-26 20:51:57 +0530

    setup done

---
+++ portfolio/components/Layout/layout.js
@@ -0,0 +1,11 @@
+
+import styled, { ThemeProvider, createGlobalStyle } from "styled-components";
+
+const Layout = ({ children }) => {
+  return (
+    <>
+    {children}
+    </>
+  );
+}
+export default Layout;
