d25b16188be621b9838a1499759f365cad0e7b4b
Vikash <yadav621gzp@gmail.com>
2023-01-08 20:52:31 +0530

    addd he nav bas compponent here

--- components/Layout/layout.js
+++ components/Layout/layout.js
@@ -1,8 +1,10 @@
 
 import styled, { ThemeProvider, createGlobalStyle } from "styled-components";
+import Navbar from "../Navbar";
 const Layout = ({ children}) => {
   return (
     <>
+    <Navbar/>
     {children}
     </>
   );
