c759922a23472e771ee42d8ea385a4ea6a4a54c5
Vikash <yadav621gzp@gmail.com>
2023-01-21 02:05:43 +0530

    added all  the commponent

--- components/Layout/layout.js
+++ components/Layout/layout.js
@@ -2,12 +2,24 @@
 import styled, { ThemeProvider, createGlobalStyle } from "styled-components";
 import Navbar from "../Navbar";
 import Footer from '../Footer'
+import Hero from '../Hero'
+import SocialLinks from '../SocialLinks'
+import Skills from '../Skills'
+import Project from '../Project'
+import Contact from '../Contact'
 const Layout = ({ children}) => {
   return (
     <>
+    <div className = "bg-gradient-to-br from-[#690a4a] via-[#100e2d] to-[#08624b]">
     <Navbar/>
+    <Hero />
+      <SocialLinks />
+      <Skills />
+      <Project />
+      <Contact />
     {children}
     <Footer />
+    </div>
     </>
   );
 }
