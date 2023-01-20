27b56b886161e61a006387ea8efea30694f200be
Vikash <yadav621gzp@gmail.com>
2023-01-21 01:42:25 +0530

    footer  added in the layout

--- components/Layout/layout.js
+++ components/Layout/layout.js
@@ -1,11 +1,13 @@
 
 import styled, { ThemeProvider, createGlobalStyle } from "styled-components";
 import Navbar from "../Navbar";
+import Footer from '../Footer'
 const Layout = ({ children}) => {
   return (
     <>
     <Navbar/>
     {children}
+    <Footer />
     </>
   );
 }
