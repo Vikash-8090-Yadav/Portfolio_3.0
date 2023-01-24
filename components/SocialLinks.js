ed911e5eb87c765f29b980bfa8a91ebe6d1039e0
Mahak <mahakgarg8002@gmail.com>
2023-01-24 18:31:30 +0530

    Files updated

--- components/SocialLinks.js
+++ components/SocialLinks.js
@@ -66,7 +66,7 @@ const SocialLinks = () => {
   ];
 
   return (
-    <div className="hidden lg:flex flex-col top-[25%] left-0 fixed">
+    <div className="hidden lg:flex flex-col top-[25%] left-0 fixed text-white">
       <ul>
         {links.map(({ id, child, href, style, download }) => (
           <li key = {id} className = {"flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500" + " " + style}>
