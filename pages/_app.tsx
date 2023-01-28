f3dc2cddacf140cfb17172aaf5e85723dcd51ca5
Vikash <yadav621gzp@gmail.com>
2023-01-28 19:16:25 +0530

    added  the loading screen

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,9 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import DotLoader from "react-spinners/HashLoader";
 import { useRouter } from 'next/router'
+import Image from "next/image";
+
 import { useEffect, useState } from 'react'
 import Layout from '../components/Layout/layout';
 import { EthereumClient, modalConnectors, walletConnectProvider } from '@web3modal/ethereum'
@@ -8,6 +11,16 @@ import { Web3Modal } from '@web3modal/react'
 import { configureChains, createClient, WagmiConfig } from 'wagmi'
 import { goerli } from 'wagmi'
 import { arbitrum, avalanche, bsc, fantom, mainnet, optimism, polygon, polygonMumbai } from 'wagmi/chains'
+import Ether from '../public/Images/Ether.png'
+// const override: CSSProperties = {
+//   text-align:"center",
+//   display :"flex",
+//   justify-content:"center",
+//   align-items:"center"
+//   borderColor: "red",
+//   width:"100%",
+//   height:"100vh",
+// };
 
 if (!"2437b6ee508a24481ec9cfa2ff6ddadf") {
   throw new Error('You need to provide NEXT_PUBLIC_PROJECT_ID env variable')
@@ -28,6 +41,13 @@ const wagmiClient = createClient({
 export const ethereumClient = new EthereumClient(wagmiClient, chains)
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [loading , setLoading] = useState(false)
+  useEffect(()=>{
+    setLoading(true)
+    setTimeout(()=>{
+      setLoading(false)
+    },5000)
+  },[])
   const { pathname } = useRouter()
   const [ready, setReady] = useState(false)
   useEffect(() => {
@@ -35,10 +55,21 @@ function MyApp({ Component, pageProps }: AppProps) {
   }, [])
   return (
     <>
-    {ready ? (
+    {loading ? (
+      <div className="load">
+    <DotLoader  color={"#36d7b7"} loading={loading}  size={71} aria-label="Loading Spinner"
+        data-testid="loader" />
+        </div>
+    )
+
+    :
+      ready ? (
       <WagmiConfig client={wagmiClient}>
+
   <Layout>
+  
     <Component {...pageProps} />
+ 
   </Layout>
   </WagmiConfig>
       ) : null}
@@ -47,6 +78,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       )}
   </>
   )
+
 }
 
 export default MyApp
